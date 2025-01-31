e2a437ed81b13dc178101134ff747185a5b4b502
Jeyner17 <jeyner.oswaldo@gmail.com>
2025-01-31 08:17:44 -0500

    new ips

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ connectDB();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:4200',
+        origin: 'https://venerable-squirrel-c3b174.netlify.app/',
         methods: ['GET', 'POST']
     }
 });
